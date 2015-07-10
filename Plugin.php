6814a9752a9caa931db4c368f4eec16fc03772c2
Pascal Kleindienst <mail@pascalkleindienst.de>
2015-07-10 16:57:34 +0200

    first init

---
+++ Plugin.php
@@ -0,0 +1,40 @@
+<?php namespace PKleindienst\BlogSearch;
+
+use System\Classes\PluginBase;
+
+/**
+ * blogSearch Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * @var array Plugin dependencies
+     */
+    public $require = ['RainLab.Blog'];
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Blog Search',
+            'description' => 'Adds a search function to the blog',
+            'author'      => 'Pascal Kleindienst',
+            'icon'        => 'icon-search'
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return [
+            'PKleindienst\BlogSearch\Components\SearchForm' => 'searchForm',
+            'PKleindienst\BlogSearch\Components\SearchResult' => 'searchResult',
+        ];
+    }
+}
