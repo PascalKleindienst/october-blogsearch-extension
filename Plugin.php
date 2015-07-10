4e77f09cddf88293e027d3de068ab719e926187e
Pascal Kleindienst <mail@pascalkleindienst.de>
2015-07-10 18:37:28 +0200

    fixed filter issue when translate plugin is not installed or disabled

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,7 @@
 <?php namespace PKleindienst\BlogSearch;
 
 use System\Classes\PluginBase;
+use System\Classes\PluginManager;
 
 /**
  * blogSearch Plugin Information File
@@ -36,5 +37,35 @@ class Plugin extends PluginBase
             'PKleindienst\BlogSearch\Components\SearchForm' => 'searchForm',
             'PKleindienst\BlogSearch\Components\SearchResult' => 'searchResult',
         ];
+    }
+
+    /**
+     * @return array
+     */
+    public function registerMarkupTags()
+    {
+        // add placebo filter if Translate Plugin does not exists, so things don't break
+        if(!PluginManager::instance()->exists('Rainlab.Translate')) {
+            return [
+                'filters' => [
+                    '_'  => [$this, 'placeboFilter'],
+                    '__' => [$this, 'placeboFilter']
+                ]
+            ];
+        }
+
+        return [];
+    }
+
+    /**
+     * Placebo Filter
+     * @param $str
+     * @param int $count
+     * @param array $params
+     * @return mixed
+     */
+    public function placeboFilter ($str, $count = 0, $params = [])
+    {
+        return $str;
     }
 }
