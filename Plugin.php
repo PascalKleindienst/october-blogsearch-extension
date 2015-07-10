a6b768773b5ca9656068b7fc61c7168bfdf08fba
Pascal Kleindienst <mail@pascalkleindienst.de>
2015-07-10 22:04:42 +0200

    removed _ filter

--- Plugin.php
+++ Plugin.php
@@ -38,34 +38,4 @@ class Plugin extends PluginBase
             'PKleindienst\BlogSearch\Components\SearchResult' => 'searchResult',
         ];
     }
-
-    /**
-     * @return array
-     */
-    public function registerMarkupTags()
-    {
-        // add placebo filter if Translate Plugin does not exists, so things don't break
-        if (!PluginManager::instance()->exists('Rainlab.Translate')) {
-            return [
-                'filters' => [
-                    '_'  => [ $this, 'placeboFilter' ],
-                    '__' => [ $this, 'placeboFilter' ]
-                ]
-            ];
-        }
-
-        return [ ];
-    }
-
-    /**
-     * Placebo Filter
-     * @param $str
-     * @param int $count
-     * @param array $params
-     * @return mixed
-     */
-    public function placeboFilter($str, $count = 0, $params = [ ])
-    {
-        return $str;
-    }
 }
