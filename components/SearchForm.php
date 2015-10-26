fe56fa898a2530f7bac84fc94aa92e8249844cbf
Pascal Kleindienst <mail@pascalkleindienst.de>
2015-10-26 17:46:01 +0100

    added dropdown for category filter

--- components/SearchForm.php
+++ components/SearchForm.php
@@ -2,6 +2,7 @@
 
 use Cms\Classes\ComponentBase;
 use Cms\Classes\Page;
+use RainLab\Blog\Models\Category as BlogCategory;
 
 /**
  * Search Form Component
@@ -13,6 +14,16 @@ class SearchForm extends ComponentBase
      * @var string Reference to the search results page.
      */
     public $resultPage;
+
+    /**
+     * @var array
+     */
+    public $categories = [];
+
+    /**
+     * @var bool
+     */
+    public $categoryFilter = false;
 
     /**
      * @return array
@@ -32,9 +43,14 @@ class SearchForm extends ComponentBase
     {
         return [
             'resultPage' => [
-                'title' => 'Search Results Page',
-                'type'=>'dropdown',
+                'title'   => 'Search Results Page',
+                'type'    => 'dropdown',
                 'default' => 'blog/search'
+            ],
+            'categoryFilter' => [
+                'title'   => 'Show Category Filter',
+                'type'    => 'checkbox',
+                'default' => 0
             ]
         ];
     }
@@ -53,5 +69,7 @@ class SearchForm extends ComponentBase
     public function onRun()
     {
         $this->resultPage = $this->page[ 'resultPage' ] = $this->property('resultPage');
+        $this->categories = $this->page[ 'categories' ] = BlogCategory::lists('name', 'id');
+        $this->categoryFilter = $this->page[ 'categoryFilter' ] = $this->property('categoryFilter');
     }
 }
